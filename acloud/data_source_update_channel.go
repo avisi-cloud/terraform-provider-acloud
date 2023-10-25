80daf7e3b2553c953e9df71dc417527f0f389f86
Thomas Kooi <t.j.kooi@avisi.nl>
2023-10-25 23:06:46 +0200

    add datasource for update channels
    Signed-off-by: Thomas Kooi <t.j.kooi@avisi.nl>

---
+++ acloud/data_source_update_channel.go
@@ -0,0 +1,59 @@
+package acloud
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/avisi-cloud/go-client/pkg/acloudapi"
+)
+
+func dataSourceUpdateChannel() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceUpdateChannelRead,
+		Schema: map[string]*schema.Schema{
+			"organisation": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Slug of the Organisation",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Name of the update channel",
+			},
+			"available": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Returns if the update channel is available",
+			},
+			"version": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Avisi Cloud Kubernetes Version associated with the Update Channel",
+			},
+		},
+	}
+}
+
+func dataSourceUpdateChannelRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(acloudapi.Client)
+	orgSlug := d.Get("organisation").(string)
+	channelName := d.Get("name").(string)
+	updateChannels, err := client.GetUpdateChannels(ctx, orgSlug)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	for _, updateChannel := range updateChannels {
+		if updateChannel.Name == channelName {
+			d.SetId(updateChannel.Name)
+			d.Set("available", updateChannel.Available)
+			d.Set("version", updateChannel.KubernetesClusterVersion)
+			return nil
+		}
+	}
+	return diag.FromErr(fmt.Errorf("update channel was not found"))
+}
